96048585862228643dd6d6ab033bf122dbf3e806
吴颖 <wuying01@sinoporf.com>
2021-06-10 18:07:18 +0800

    拖拽页面

--- demo.js
+++ demo.js
@@ -1,8 +1,115 @@
 var app = new Vue({
-    el: '#app',
-    data(){
+    el: "#app",
+    data() {
         return {
-
-        }
+            originData: [
+                {
+                    name: "Page",
+                    class: "form_page",
+                    isSelected: false,
+                    property: {
+                        title: "页面",
+                    },
+                    event: {},
+                },
+                {
+                    name: "Row",
+                    class: "form_row",
+                    grid: 2,
+                    gutter: 10,
+                },
+                {
+                    name: "Navbar",
+                },
+                {
+                    name: "Form",
+                },
+                {
+                    name: "Button Group",
+                },
+                {
+                    name: "Button",
+                },
+                {
+                    name: "Section",
+                },
+                {
+                    name: "Input",
+                },
+                {
+                    name: "Hidden",
+                },
+                {
+                    name: "Table",
+                },
+                {
+                    name: "Columns",
+                },
+                {
+                    name: "Details",
+                },
+                {
+                    name: "Text",
+                },
+                {
+                    name: "Details",
+                },
+                {
+                    name: "Text",
+                },
+                {
+                    name: "TextArea",
+                },
+                {
+                    name: "Checkboxes",
+                },
+                {
+                    name: "Checkbox",
+                },
+                {
+                    name: "Radios",
+                },
+                {
+                    name: "Select",
+                },
+                {
+                    name: "Option",
+                },
+                {
+                    name: "DatePicket",
+                },
+            ],
+            id: 0,
+            items: [],
+            droppedItem: "",
+        };
     },
-  })
+    methods: {
+        add(item) {
+            console.log("🚀 ~ file: demo.js ~ line 81 ~ add ~ item", item);
+            this.id++;
+            if(item.isSelected){
+                item.class=item.class+' isSelected'
+            }
+            if (item.name == "Page") {
+                this.droppedItem += `
+                <div class="${item.class}" id='${this.id}'>
+                    <h4>${item.property.title}</h4>
+                    <div class=""></div>
+                </div>
+                `;
+                console.log(this.droppedItem);
+            }
+        },
+        dragstart(e, item) {            
+            this.add(item);
+            // this.droppedItem = e.dataTransfer.getData('item')
+        },
+        dragend(e, item) {
+            e.dataTransfer.setData("item", item.name);
+        },
+        drop(e) {
+            e.dataTransfer.clearData();
+        },
+    },
+});
